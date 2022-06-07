a4eea58cce70c9edf0cb1c91673b1d8935390dd5
SirHunJang <choi961005@naver.com>
2022-06-07 18:49:10 +0900

    compiler environment test

--- src/study/Cklim.java
+++ src/study/Cklim.java
@@ -1,10 +1,22 @@
 package study;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cklim {
 	public static void main(String[] args) {
     String name = "shim jae rin";
     int i = 29;
     String job = "developer";
-    
+
+        List<String> list = new ArrayList<>();
+        for (int j = 0; j < 10; j++) {
+            list.add(j + "test");
+        }
+
+        list.forEach(item -> {
+            System.out.println(item);
+        });
+
 	}
 }
